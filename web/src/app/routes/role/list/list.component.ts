790fbb9db2b9868e8613d68b7db29fe338f0c8cd
devcui <devcui@outlook.com>
2023-03-14 21:28:56 +0800

    feat(web): add role crud

--- web/src/app/routes/role/list/list.component.ts
+++ web/src/app/routes/role/list/list.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit, ViewChild } from '@angular/core';
 import { STColumn, STComponent } from '@yelon/abc/st';
-import { ModalHelper, _HttpClient } from '@yelon/theme';
+import { _HttpClient } from '@yelon/theme';
 
 import { RoleService } from '../role.service';
 
@@ -13,29 +13,79 @@ import { RoleService } from '../role.service';
       </ng-template>
     </page-header>
     <nz-card>
-      <st #st [data]="data" [columns]="columns"></st>
+      <st #st [data]="data" [loading]="loading" [columns]="columns"></st>
     </nz-card>
   `
 })
 export class RoleListComponent implements OnInit {
   data = [];
+  loading = false;
   @ViewChild('st') private readonly st!: STComponent;
   columns: STColumn[] = [
     { title: '角色名称', type: '', index: 'role_name' },
     { title: '角色值', type: '', index: 'role_value' },
     {
       title: '',
-      buttons: [{ text: '编辑' }, { text: '删除' }]
+      buttons: [
+        {
+          text: '编辑',
+          click: role => {
+            this.update(role);
+          }
+        },
+        {
+          text: '删除',
+          click: role => {
+            this.delete(role.id);
+          }
+        }
+      ]
     }
   ];
 
-  constructor(private http: _HttpClient, private modal: ModalHelper, private roleService: RoleService) {}
+  constructor(private roleService: RoleService) {}
 
   ngOnInit(): void {
+    this.query();
+  }
+
+  query(): void {
+    this.loading = true;
     this.roleService.query().subscribe((response: any) => {
       this.data = response.body;
+      this.loading = false;
     });
   }
 
-  add(): void {}
+  update(role: any): void {
+    const id = role.id;
+    this.roleService.createForm(role => {
+      role.id = id;
+      return this.roleService
+        .update(role)
+        .toPromise()
+        .then(() => {
+          this.query();
+        });
+    }, role);
+  }
+
+  add(): void {
+    this.roleService.createForm(role => {
+      return this.roleService
+        .add(role)
+        .toPromise()
+        .then(() => {
+          this.query();
+        });
+    }, null);
+  }
+
+  delete(id: string): void {
+    this.loading = true;
+    this.roleService.delete(id).subscribe(() => {
+      this.loading = false;
+      this.query();
+    });
+  }
 }
--- web/src/app/routes/role/role.service.ts
+++ web/src/app/routes/role/role.service.ts
@@ -1,15 +1,76 @@
 import { Injectable, OnDestroy } from '@angular/core';
 import { environment } from '@env/environment';
+import { SFComponent, SFSchema, SFValue, SFValueChange } from '@yelon/form';
 import { _HttpClient } from '@yelon/theme';
-import { Observable, Subject } from 'rxjs';
+import { NzModalRef, NzModalService } from 'ng-zorro-antd/modal';
+import { Observable, Subject, takeUntil } from 'rxjs';
 
 @Injectable({ providedIn: 'root' })
 export class RoleService implements OnDestroy {
   private destroy$ = new Subject<void>();
-  constructor(private http: _HttpClient) {}
+  constructor(private http: _HttpClient, private modal: NzModalService) {}
+
+  createForm(nzOnOk: (param: any) => Promise<any>, data?: any): void {
+    const schema: SFSchema = {
+      properties: {
+        role_name: {
+          type: 'string',
+          ui: {
+            i18n: 'role_name',
+            widget: 'string'
+          }
+        },
+        role_value: {
+          type: 'string',
+          ui: {
+            i18n: 'role_value',
+            widget: 'string'
+          }
+        }
+      }
+    };
+    let formValue: SFValue = {};
+    const ref: NzModalRef<SFComponent, any> = this.modal.create<SFComponent>({
+      nzTitle: '角色管理',
+      nzMask: true,
+      nzClosable: true,
+      nzMaskClosable: true,
+      nzContent: SFComponent,
+      nzComponentParams: {
+        button: null,
+        formData: data
+      },
+      nzOnOk: () => {
+        let valid: boolean | undefined = ref.componentInstance?.validator({ emitError: true, onlyRoot: false });
+        if (!valid) {
+          return Promise.reject(false);
+        } else {
+          return nzOnOk(formValue);
+        }
+      }
+    });
+    ref.componentInstance?.formValueChange.pipe(takeUntil(this.destroy$)).subscribe((sfValueChange: SFValueChange) => {
+      formValue = sfValueChange.value;
+    });
+    ref.componentInstance?.refreshSchema(schema);
+    // hack: use reset to trigger formValueChange function
+    ref.componentInstance?.reset();
+  }
 
   query(): Observable<any> {
     return this.http.get(`${environment['path']}/role`);
+  }
+
+  add(role: any): Observable<any> {
+    return this.http.post(`${environment['path']}/role`, role);
+  }
+
+  delete(id: string): Observable<any> {
+    return this.http.delete(`${environment['path']}/role/${id}`);
+  }
+
+  update(role: any): Observable<any> {
+    return this.http.put(`${environment['path']}/role`, role);
   }
 
   ngOnDestroy(): void {
